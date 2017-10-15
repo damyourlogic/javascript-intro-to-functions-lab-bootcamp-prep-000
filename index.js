2abbadc85808f1e81d54ca54efc20d312a71cd6c
Amy Jackson <aj81623@gmail.com>
2017-10-15 23:14:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   }else if (string.toUpperCase()===string) {
     return "Stop shouting at me!"
-  }else (string ===string) {
+  }else () {
     return "I love you too"
   }
 }
