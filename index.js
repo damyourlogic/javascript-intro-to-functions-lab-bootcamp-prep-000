90b36f79944ea7976e302be9491ec6c70d226187
Amy Jackson <aj81623@gmail.com>
2017-10-15 22:43:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function shout(string) {
   return string.toUpperCase()
 }
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log (string.toUpperCase())
+}
+function logWhisper(string){
+  console.log (string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if (string.toLowerCase('hello')){
+    return string.toMixedCase("I can't hear you!")
+  }
+}
